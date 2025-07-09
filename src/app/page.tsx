0134af8481e3890b51314eda9312768134adbf46
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-09 13:22:44 +0530

    feat: update dependencies and modify authentication flow
    - Added new dependency "auth" to package.json.
    - Removed the Dashboard component from the app.
    - Updated Home component to redirect unauthenticated users to the login page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
+
+  const { data: session } = useSession();
+
+  // if (status === "loading") {
+  //   return <p>Loading...</p>;
+  // }
+
+  if (!session) {
+    window.location.href = "/login";
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
